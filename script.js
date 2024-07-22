4a4d5cfdcbf6ad604bd82051259d695dd010ccae
Jeppe Hesselgren <jeppehesselgren@gmail.com>
2024-07-22 22:26:10 +0200

    Finshed last touches

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 const clearAllFields = document.getElementById("clearAll");
 const sumbitForm = document.getElementById("data-form");
 
+const test = document.getElementsByClassName("root");
+
+const html = document.querySelector("body");
+
 
 //Add all eventListeners and the eventhandlers from the function.js file
 clearAllFields.addEventListener("click", clearAll);
